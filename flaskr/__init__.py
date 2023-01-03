132698f5bd02c8566a75bef773ae0d35bac2b03d
Raxorty <119407107+Raxorty@users.noreply.github.com>
2023-01-03 11:24:19 +0100

    a full on update with blank spots for the database info!

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,50 +1,57 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+import mysql.connector
 
 app = Flask(__name__)
+#Hier geven we de secret key aan, deze is nodig voor het gebruik van sessies en encryptie en decryptie.
 app.secret_key = "BrunoBroodje"
-app.permanent_session_lifetime = timedelta(minutes=5)
+#De maximale tijd dat data mag worden opgeslagen in een sessie.
+app.permanent_session_lifetime = timedelta(days=1)
 
-@app.route("/")
-def landingspage():
-	return render_template("landingspage.html")
+cnx = mysql.connector.connect(user='your_username',
+                             password='your_password',
+                             host='your_host',
+                             database='your_database')
 
-@app.route("/entertainmentpage")
-def entertainmentpage():
-    return render_template("entertainmentpage.html")
+cursor = cnx.cursor()
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        session.permanent = True
-        user = request.form["nm"]
-        session["user"] = user
-        flash("Login Succesful!")
-        return redirect(url_for("user"))
-    else:
-        if "user" in session:
-            flash("Already Logged In!")
-            return redirect(url_for("user"))
-            
-        return render_template("login.html")
+        username = request.form["username"]
+        password = request.form["password"]
 
-@app.route("/user")
-def user():
-    if "user" in session:
-        user = session["user"]
-        return render_template("user.html", user=user)
+        #Query de database voor de 2 waarden die nodig zijn om in te mogen loggen.
+        cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+        user = cursor.fetchone()
+
+        #Als de login correct is mag je door naar de landingspagina.
+        if user is not None:
+            session["username"] = username
+            return redirect("/landingspagina")
+        
+        #Als de login niet kloppend is word er een waarschuwing gegeven.
+        else:
+            flash("Login failed, try again.")
+            return render_template("login.html")
+    
+    #Als de request method GET is word de login pagina weergeven.
+    return render_template("login.html")
+        
+@app.route("/landingspagina")
+def landingspagina():
+    #Checkt of de user is ingelogd.
+    if "username" in session:
+        return render_template("landingspagina.html", username=session["username"])
     else:
-        flash("You Are Not Logged In!")
-        return redirect(url_for("login"))
+        return redirect("/login")
 
 @app.route("/logout")
 def logout():
-    if "user" in session:
-        user = session["user"]
-        flash(f"You Have Been Logged Out, {user}", "info")
-    session.pop("user", None)
-    return redirect(url_for("login"))
-    
+    #Haalt de user weg van de sessie.
+    session.pop("username", None)
+    return redirect("/login")
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
